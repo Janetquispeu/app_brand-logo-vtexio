42637a87980441481de0780296e9e961fbd41989
Janet Quispe <janet.quispe@irdigital.pe>
2020-08-04 11:56:53 -0500

    feat(all): componente Brand Logo

--- react/BrandLogo.tsx
+++ react/BrandLogo.tsx
@@ -1,45 +1,17 @@
 import PropTypes from 'prop-types'
-import React, { Component } from 'react';
-// import { ProductContext } from 'vtex.product-context';
-import { path, any } from 'ramda';
-// import { useProductSummary } from 'vtex.product-summary-context/ProductSummaryContext';
+import React from 'react';
 
 interface IProps {
   image: string;
-  query: any;
 }
 
-const hasSellerWithAvailableItems = (item: any) => {
-  return item.sellers && any((params: any) => params.commertialOffer && params.commertialOffer.AvailableQuantity > 0, item.sellers)
-}
-
-export class BrandLogo extends Component<IProps, {}> {
+export class BrandLogo extends React.Component<IProps, {}> {
   static propTypes = {
-    image: PropTypes.string,
-    query: PropTypes.shape({
-      skuId: PropTypes.string,
-    }),
-  }
-
-  get selectedItem() {
-    const items: Array<any> = path(['productQuery', 'product', 'items'], this.props) || []
-    if (!this.props.query || !this.props.query.skuId) {
-      return items.find(hasSellerWithAvailableItems) || items[0]
-    }
-    return items.find((sku: any) => sku.itemId === this.props.query.skuId)
-  }
-
-  get commertialOffer() {
-    return path(['sellers', 0, 'commertialOffer'], this.selectedItem)
+    image: PropTypes.string
   }
 
   render () {
-    const {
-      image
-    } = this.props;
-    
-    // const productContext = useContext(ProductContext)
-    console.log(this.commertialOffer);
+    const { image } = this.props;
 
     return (
       <div>
